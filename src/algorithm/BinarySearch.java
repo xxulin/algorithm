876e768492f7493e7d278693ce817a014832a16b
xullin <497120555@qq.com>
2020-04-17 10:24:50 +0800

    提交基本二分查找写法，及四种变体问题。二分查找更适合查找“近似”问题。

--- src/algorithm/BinarySearch.java
+++ src/algorithm/BinarySearch.java
@@ -9,10 +9,10 @@ public class BinarySearch {
 
 	public static void main(String[] args) {
 		
-		int[] a = {0, 1, 2, 3, 5, 8, 10, 16, 19};
+		int[] a = {0, 0, 1, 2, 5, 5, 8, 10, 16, 16};
 		for(int i = 0; i < a.length; i++) {
 			int target = a[i];
-			System.out.println(binarySearch(a, 0, a.length - 1, target));
+			System.out.println(searchLast(a, target));
 		}
 		
 	}
@@ -70,6 +70,141 @@ public class BinarySearch {
 		} else {
 			return binarySearch(a, mid + 1, high, target);
 		}
+		
+	}
+	/**
+	 * 变体：查找第一个值等于给定值的元素
+	 * @param a
+	 * @param target
+	 * @return
+	 */
+	public static int searchFirst(int[] a, int target) {
+		
+		int low = 0;
+		int high = a.length - 1;
+		int mid = -1;
+		
+		while(low <= high) {
+			
+			mid = low + ((high - low) >> 1);
+			if(a[mid] > target) {
+				high = mid - 1;
+			} else if(a[mid] < target) {
+				low = mid + 1;
+			} else {
+				
+				if(mid == 0 || a[mid - 1] != target) {
+					return mid;
+				} else {
+					high = mid - 1;
+				}
+				
+			}
+			
+		}
+		
+		return -1;
+	}
+	
+	/**
+	 * 变体：查找最后一个值等于给定值的元素
+	 * @param a
+	 * @param target
+	 * @return
+	 */
+	public static int searchLast(int[] a, int target) {
+		
+		int low = 0;
+		int high = a.length - 1;
+		int mid = -1;
+		
+		while(low <= high) {
+			
+			mid = low + ((high - low) >> 1);
+			if(a[mid] > target) {
+				high = mid - 1;
+			} else if(a[mid] < target) {
+				low = mid + 1;
+			} else {
+				
+				if(mid == a.length - 1 || a[mid + 1] != target) {
+					return mid;
+				} else {
+					low = mid + 1;
+				}
+				
+			}
+			
+		}
+		
+		return -1;
+		
+	}
+	
+	/**
+	 * 查找第一个大于等于给定值的元素
+	 * @param a
+	 * @param target
+	 * @return
+	 */
+	public static int searchFirstGE(int[] a, int target) {
+		
+		int low = 0;
+		int high = a.length - 1;
+		int mid = -1;
+		
+		while(low <= high) {
+			
+			mid = low + ((high - low) >> 1);
+			if(a[mid] >= target) {
+				
+				if(mid == 0 || a[mid - 1] < target) {
+					return mid;
+				} else {
+					high = mid - 1;
+				}
+				
+			} else {
+				low = mid + 1;
+			}
+			
+		}
+		
+		return -1;
+	}
+	
+	/**
+	 * 查找最后一个小于等于给定值的元素
+	 * @param a
+	 * @param target
+	 * @return
+	 */
+	public static int searchLastLE(int[] a, int target) {
+		
+		int low = 0;
+		int high = a.length - 1;
+		int mid;
+		
+		while(low <= high) {
+			
+			mid = low + ((high - low) >> 1);
+			if(a[mid] <= target) {
+				
+				if(mid == a.length - 1 || a[mid + 1] > target) {
+					return mid;
+				} else {
+					low = mid + 1;
+				}
+				
+			} else {
+				
+				high = mid - 1;
+				
+			}
+			
+		}
+		
+		return -1;
 		
 	}
 	
